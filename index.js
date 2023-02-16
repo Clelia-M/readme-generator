b535ee83c8cc568ef92a7ef2c20a36e027afb759
Clelia-M <114869080+Clelia-M@users.noreply.github.com>
2023-02-16 20:34:35 +0000

    installation prompt question & pseudocode

--- index.js
+++ index.js
@@ -6,15 +6,42 @@ const generateMarkdown = require("./utils/generateMarkdown");
 // array of questions for user
 const questions = [
 {
+    // The title of my project
     type: 'input',
     name: 'title',
     message: 'What is the title of your Project?'
 }, 
-{
+
+{ // Description
     type: 'input',
     name: 'description',
     message: 'Please provide a description of your Project:', 
 }, 
+
+{
+    //Table of Contents
+},
+
+{ // Installation
+    type: 'input',
+    name: 'installation', 
+    message: 'Please provide installation instructions for your Project:'
+},
+{
+    // Usage
+},
+{
+    // License
+}, 
+{
+    // Contributing
+},
+{
+    // Tests
+}, 
+{
+    // Questions
+}
 ];
 
 // function to write README file
